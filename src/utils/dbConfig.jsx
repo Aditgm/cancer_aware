0923a258b5ed8b62e54c240049a8cd5a4aa9d0bc
Aditgm <adityagithubgm@gmail.com>
2025-07-06 00:47:54 +0530

    Update dbConfig.jsx

--- src/utils/dbConfig.jsx
+++ src/utils/dbConfig.jsx
@@ -4,7 +4,7 @@ import * as schema from "./schema";
 
 const sql = neon(
   import.meta.env.VITE_DATABASE_URL || 
-  "postgresql://finan-smart_owner:uk3aed9QZotj@ep-wispy-breeze-a5iadk8t.us-east-2.aws.neon.tech/beat-cancer?sslmode=require"
+  "postgresql://finan-smart_owner:uk3aed9Q0Zotj@ep-wispy-breeze-a5iadk8t.us-east-2.aws.neon.tech/cancer-aware?sslmode=require"
 );
 
 export const db = drizzle(sql, { schema });
