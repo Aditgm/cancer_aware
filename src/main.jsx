c6c9dccfc3acf40ff7fadb9f89194d74983bbb18
Aditgm <adityagithubgm@gmail.com>
2025-07-05 17:15:17 +0530

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import { StateContextProvider } from "./context";
+import App from "./App";
+import "./index.css";
+import { PrivyProvider } from "@privy-io/react-auth";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <PrivyProvider
+    appId="clxt5gya607hj5gfcjmhvi6iw"
+    config={{
+      appearance: {
+        theme: "dark",
+        logo: "https://ibb.co/vCP9TkQ0g",
+      },
+      embeddedWallets: {
+        createOnLogin: "users-without-wallets",
+      },
+    }}
+  >
+    <Router>
+      <StateContextProvider>
+        <App />
+      </StateContextProvider>
+    </Router>
+  </PrivyProvider>
+);
