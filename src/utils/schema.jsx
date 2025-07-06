c3a1e89c6ece888f9ca06431a0183a4e9e55ce3c
Aditgm <adityagithubgm@gmail.com>
2025-07-06 14:56:57 +0530

    Update schema.jsx

--- src/utils/schema.jsx
+++ src/utils/schema.jsx
@@ -1,6 +1,7 @@
 import { sql } from "drizzle-orm";
 import { integer, varchar, pgTable, serial, text, jsonb, timestamp } from "drizzle-orm/pg-core";
 
+// users schema
 export const Users = pgTable("users", {
   id: serial("id").primaryKey(),
   username: varchar("username").notNull(),
@@ -21,6 +22,7 @@ export const Users = pgTable("users", {
   cancerType: varchar("cancer_type", { length: 100 }),
 });
 
+// records schema
 export const Records = pgTable("records", {
   id: serial("id").primaryKey(),
   userId: integer("user_id")
@@ -28,10 +30,14 @@ export const Records = pgTable("records", {
     .notNull(),
   recordName: varchar("record_name").notNull(),
   analysisResult: varchar("analysis_result").notNull(),
+  analysisData: jsonb("analysis_data"),
   kanbanRecords: varchar("kanban_records").notNull(),
   createdBy: varchar("created_by").notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  files: jsonb("files").notNull().default([]),
 });
 
+// kanban boards schema
 export const KanbanBoards = pgTable("kanban_boards", {
   id: serial("id").primaryKey(),
   userId: integer("user_id").references(() => Users.id).notNull(),
